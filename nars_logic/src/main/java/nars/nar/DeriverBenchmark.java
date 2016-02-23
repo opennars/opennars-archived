22bce6392dcfe3451d695f1629434fb8cf7ff86b
patrick.hammer <patham9@gmail.com>
2016-02-23 15:22:17 +0100

    with list

--- nars_logic/src/main/java/nars/nar/DeriverBenchmark.java
+++ nars_logic/src/main/java/nars/nar/DeriverBenchmark.java
@@ -3,12 +3,15 @@ package nars.nar;
 import nars.NAR;
 import nars.process.TaskBeliefProcess;
 import nars.task.Task;
+import nars.util.data.list.FasterList;
+
+import java.util.List;
 
 /**
  * Created by patrick.hammer on 23.02.2016.
  */
 public class DeriverBenchmark {
-
+    final static List<Task> derivedTasksBuffer = new FasterList();
     public static void main(String[] args) {
         final Default nar = new Default();
         final Task t1 = nar.inputTask("<chess --> competition>.");
@@ -16,9 +19,11 @@ public class DeriverBenchmark {
 
         TaskBeliefProcess tbp = new TaskBeliefProcess(nar, t1, t2);
 
+
         long startTime = System.nanoTime();
         for(int i=0;i<25000;i++) {
-            nar.getDeriver().run(tbp, c -> {});
+            nar.getDeriver().run(tbp, derivedTasksBuffer::add);
+            derivedTasksBuffer.clear();
         }
         long endTime  = System.nanoTime();
         long ms = (endTime - startTime) / 1000000;
