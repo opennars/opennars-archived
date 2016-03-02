540b8fa1c405f198ed6211d2d9a775dc6dc72e6c
patrick.hammer <patham9@gmail.com>
2016-03-02 16:32:01 +0100

    experience recording.

--- nars_gui/src/main/java/nars/guifx/demo/NARfxDefaultIterative.java
+++ nars_gui/src/main/java/nars/guifx/demo/NARfxDefaultIterative.java
@@ -1,23 +1,39 @@
 package nars.guifx.demo;
 
+import nars.NAR;
 import nars.guifx.NARide;
 import nars.nar.Default;
+import nars.task.Task;
 
-/**
- * Created by me on 9/7/15.
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class NARfxDefaultIterative {
 
+    static String recordfile = "C://myfile.txt";
+
     public static void main(String[] arg) {
+        NARfxDefaultIterative def = new NARfxDefaultIterative();
+        NAR nar = new Default(1000, 1, 1, 3);
+       // nar.memory.eventInput.on(def::yourNewTaskTrackMethod);
+        NARide.show(nar.loop(), (i) -> {});
+    }
 
-        NARide.show(new Default(1000, 1, 1, 3).loop(), (i) -> {
-            /*try {
-                i.nar.input(new File("/tmp/h.nal"));
-            } catch (Throwable e) {
-                i.nar.memory().eventError.emit(e);
-                //e.printStackTrace();
-            }*/
-        });
-
+    public void yourNewTaskTrackMethod(Task t) {
+        try {
+            Files.write(Paths.get(recordfile), "".getBytes(), StandardOpenOption.CREATE_NEW); //create new if exists not already
+        }catch(IOException ex) {}
+        try {
+            String wr = t.toString();
+            if(t.isInput()) {
+                wr = "IN: "+wr;
+            } else {
+                wr = "OUT: "+wr;
+            }
+            wr = wr + "\n";
+            Files.write(Paths.get(recordfile), wr.getBytes(), StandardOpenOption.APPEND); //ok we can write into
+        }catch (IOException e) {}
     }
 }
