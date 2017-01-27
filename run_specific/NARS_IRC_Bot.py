4bb412d5753d2d360c241d04ddbc9dfcd334c608
PtrMan <robertw89@googlemail.com>
2017-01-27 23:46:56 +0100

    irc bot enhancements

--- run_specific/NARS_IRC_Bot.py
+++ run_specific/NARS_IRC_Bot.py
@@ -1,4 +1,4 @@
-import thread 
+import thread
 from subprocess import Popen, PIPE, STDOUT
 import subprocess
 import re,os
@@ -9,13 +9,19 @@ import itertools
 
 knowledge=""""""
 
+# is the nars package compiled as a public release version and moved to the root?
+runInReleaseEnvironment = False
+
 def init_proc():
     global proc
-    proc = subprocess.Popen(["java","-cp","OpenNARS.jar","nars.io.NARConsole","1"], stdin=subprocess.PIPE, stdout=subprocess.PIPE) 
+    if runInReleaseEnvironment:
+        proc = subprocess.Popen(["java","-cp","OpenNARS.jar","nars.io.NARConsole","1"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    else:
+        proc = subprocess.Popen(["java","-cp", "lib\\*;dist\\*", "nars.io.NARConsole"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 server = "irc.freenode.net"
 channel = "#nars"
-botnick = "mr_nars"
+botnick = "nars_squared"
 Narsese_Filter=["EXE: ","Answer:"]
 Max_Outputs_Before_Reset=30
 
@@ -25,17 +31,17 @@ irc.send("USER "+ botnick +" " + botnick +" "+ botnick +" :Hallo!\r\n")
 irc.send("NICK "+ botnick +"\r\n")
 irc.send("PRIVMSG nickserv :iNOOPE\r\n")
 irc.send("JOIN "+ channel +"\r\n") #fails on euirc, works in all others, euirc is not completely RFC 2810 compatible maybe?
-print "connected"
+print("connected")
 init_proc()
-print "NARS instance created"
+print("NARS instance created")
 proc.stdin.write("*volume=0\n");
-print "NARS is set to shut up: "+proc.stdout.readline()
-print "Narsese Filter applied: '"+str(Narsese_Filter)+"'"
-print "Max Outputs before Reset: "+str(Max_Outputs_Before_Reset)
+print("NARS is set to shut up: "+proc.stdout.readline())
+print("Narsese Filter applied: '"+str(Narsese_Filter)+"'")
+print("Max Outputs before Reset: "+str(Max_Outputs_Before_Reset))
 cnt=0
 
 proc.stdin.write(knowledge)
-print "Knowledge put in"
+print("Knowledge put in")
 
 def receive_thread(a):
     global cnt
@@ -45,7 +51,7 @@ def receive_thread(a):
            msg=msg.split("% {")[0]+"%"
        if msg!=None and msg!="" and True in [u in msg for u in Narsese_Filter]: #we received an execution
           cnt+=1
-          print "NAR output: "+msg
+          print("NAR output: "+msg)
           bReset= cnt>=Max_Outputs_Before_Reset
           irc.send("PRIVMSG "+ channel +" : "+msg+"\r\n")
           if bReset:
@@ -55,41 +61,45 @@ def receive_thread(a):
               #init_proc();
               proc.stdin.write("*****\n")
               proc.stdin.write(knowledge)
-              print "Knowledge put in"
-          
+              print("Knowledge put in")
+
 thread.start_new_thread(receive_thread,(1,))
 
 while True:
     try:
         text=irc.recv(2040)
         if "PING" in text:
-            print "ping"
+            print("ping")
             STR='PONG :' + text.split("PING :")[1].split("\n")[0] + '\r\n';
             irc.send(STR)
         else:
             if "VERSION" in text:
-                print "version"
+                print("version")
                 irc.send("JOIN "+ channel +"\r\n") #join when version private message comes :D
             else:
                 if "system" in text.lower() or "from os" in text.lower() or "import os" in text.lower():
-                    print "skipped"
+                    print("skipped")
                     continue
                 SPL=text.split(":")
                 TEXT=":".join(SPL[2:len(SPL)])
                 if TEXT.replace(" ","").replace("\n","").replace("\r","")=="":
-					continue
-                print TEXT
-                if TEXT.startswith("**"):
+                    continue
+                print(TEXT)
+                if TEXT[:2] == "**":
                     proc.stdin.write("*****\n")
                     proc.stdin.write(knowledge)
-                    print "Knowledge put in"
-                if TEXT.startswith("<") or TEXT.startswith("("):
-                    print "NAR input: "+TEXT
+                    print("Knowledge put in")
+                elif TEXT[0] == "<" or TEXT[0] == "(":
+                    print("NAR input: "+TEXT)
                     try:
                         proc.stdin.write(TEXT+"\n")
                     except:
                         None
-                
+                elif TEXT[0] == "+":
+                    proc.stdin.write("+"+TEXT+"\n")
+                elif TEXT[:] == "*pause":
+                    proc.stdin.write("+"+TEXT+"\n")
+
     except:
-        print "exception"
+        print("exception")
         None
